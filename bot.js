40f7e77e36df2be40c812e43514f2749622503a7
illusiveo <46373282+illusiveo@users.noreply.github.com>
2019-02-07 15:47:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '+'
 
 
-const developers = ["527562287062581259","527562287062581259"]
+const developers = ["229192961907228674","229192961907228674"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
